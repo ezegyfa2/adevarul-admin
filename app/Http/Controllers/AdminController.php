497cd9ea9d7ca8c89190b44966a79cea0576134f
Darvas Istvan <darvasvok@gmail.com>
2023-05-12 10:54:42 +0300

    Changing admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Ezegyfa\LaravelHelperMethods\Crm\Controllers\BlueAdminController;
+
+class AdminController extends BlueAdminController
+{
+    public $indexTemplateName = 'data_collector_index';
+    public $editTemplateName = 'data_collector_edit';
+    public $templateFolderPath = __DIR__ . '\\Templates';
+    public $filterFormItemPrefix = 'data-collector-filter';
+}
